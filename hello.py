158bdd45a2b3b7048ce8fb429d6c84da61f179aa
kaungmyatthymdy <khathtan3131997@gmail.com>
2018-06-20 12:47:52 +0630

    Added hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
-app = Flask("/")
+app = Flask('/')
  
 def home():
     return "Hello World!"
 
 
-app.route("/index")
+app.route('/index')
 def index():
-    return "This is index"
+    return "This is index."
 
 if __name__ == "__main__":
     app.run(debug=True)
